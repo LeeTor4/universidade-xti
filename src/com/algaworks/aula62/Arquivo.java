cc213d7829bf262ebf6b6516f1222c6358048c9d
Francisco Lee Torquato Aragao <leetor4@gmail.com>
2019-10-06 10:53:44 -0300

    Exercicios Path

---
+++ src/com/algaworks/aula62/Arquivo.java
@@ -0,0 +1,19 @@
+package com.algaworks.aula62;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Arquivo {
+
+	public static void main(String[] args) {
+		
+		//C:\Users\Lee_2\Documents\ORTOGENESE\SPED\2014
+		Path path = Paths.get("C:/Users/Lee_2/Documents/ORTOGENESE/SPED/2014/SPED_201401.txt");
+		System.out.println(path.getParent());
+		System.out.println(path.getFileName());
+		System.out.println(path.toAbsolutePath());
+		
+		
+	}
+
+}
---
+++ src/com/algaworks/aula66/Arquivo.java
@@ -0,0 +1,29 @@
+package com.algaworks.aula66;
+
+import java.nio.file.DirectoryIteratorException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Arquivo {
+
+	public static void main(String[] args) {
+		
+		Path dir = Paths.get("C:/Users/Lee_2/Documents/ORTOGENESE/SPED/2014/");
+
+		/*LISTAR O CONTEÚDO*/
+		try (DirectoryStream<Path> stream =  Files.newDirectoryStream(dir)){
+			
+			for(Path path : stream) {
+				
+				System.out.println(path.getFileName());
+				
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
